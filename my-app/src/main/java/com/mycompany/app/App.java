e7bd79574e4cdc5809f55012303b241ceaa45716
selfint <40059156+selfint@users.noreply.github.com>
2023-08-27 15:01:50 +0000

    add java project

---
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,14 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String secret = "The answer to life is 42";
+        System.out.println( "The secret is: " + secret );
+    }
+}
